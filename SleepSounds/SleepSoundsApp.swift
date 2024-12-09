2681fb9f70cd82b8f58a7b07662c2a6bd745e121
JiyanAtalay <jyntly23@gmail.com>
2024-12-09 13:22:57 +0300

    Initial Commit

---
+++ SleepSounds/SleepSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepSoundsApp.swift
+//  SleepSounds
+//
+//  Created by Mehmet Jiyan Atalay on 9.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SleepSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
